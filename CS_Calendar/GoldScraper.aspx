62fbd5b8d79bfd0db85197c3a2cfa7510592ce70
stefanagloginic - Stefana Gloginic <stefana.gloginic@gmail.com>
2016-12-18 01:01:50 -0800

    Added PhantomJs code which is used to scrape gold for registration pass times. Basic login html is included.

---
+++ CS_Calendar/GoldScraper.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GoldScraper.aspx.cs" Inherits="CS_Calendar.GoldScraper" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <div class ="form">
+        <form class="loginform"  runat="server">
+            <input type="text" id="inputUsername" runat="server" placeholder="Username"/>
+            <input type="password" id="inputPassword" runat="server" placeholder="Password"/>
+            <asp:Button ID="Button1" runat="server" Text="Log In" OnClick="Button1_Click1" />
+            <p id="registertext">Not Registered? <a href="javascript:void(0)">Sign Up</a></p>
+        </form>
+    </div>
+</body>
+</html>
---
+++ CS_Calendar/GoldScraper.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Web.UI.WebControls;
+
+namespace CS_Calendar
+{
+    public partial class GoldScraper : System.Web.UI.Page
+    {
+        public static string BaseUrl = "https://my.sa.ucsb.edu/gold/login.aspx";
+        public static string regInfoUrl = "https://my.sa.ucsb.edu/gold/RegistrationInfo.aspx";
+        public const int TimeOut = 30;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Login()
+        {
+            string user = inputUsername.Value.ToString();
+            string pass = inputPassword.Value.ToString();
+
+            var driver = new OpenQA.Selenium.PhantomJS.PhantomJSDriver(); //headless browser to navigate to gold
+            driver.Navigate().GoToUrl(BaseUrl);
+
+            var loginBox = driver.FindElementById("pageContent_userNameText"); //grab reference to username textfield on gold login
+            loginBox.SendKeys(user);
+
+            var pwBox = driver.FindElementById("pageContent_passwordText");//grab reference to password textfield on gold login
+            pwBox.SendKeys(pass);
+
+            var signinBtn = driver.FindElementById("pageContent_loginButton"); //grab reference to  login btn on gold login
+            signinBtn.Click();
+
+            var registrationImg = driver.FindElementById("ctl09_image"); //grab reference to registration for navigation 
+            registrationImg.Click();
+
+            IWebElement elem1 = driver.FindElementById("pageContent_PassOneLabel");
+            IWebElement elem2 = driver.FindElementById("pageContent_PassTwoLabel");
+            IWebElement elem3 = driver.FindElementById("pageContent_PassThreeLabel");
+
+            List<string> registrationDates = new List<string>();
+
+            registrationDates.Add(elem1.Text);
+            registrationDates.Add(elem2.Text);
+            registrationDates.Add(elem3.Text);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Response.Write(registrationDates[i] + " ");
+            }
+
+            driver.Quit();
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            Login();
+        }
+    }
+}
