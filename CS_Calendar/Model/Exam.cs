1ed79ddca8492b576d71a70042b02faa35d03e25
Angel Rivera <wrathchild4213@gmail.com>
2017-01-22 01:46:13 -0800

    Added a function to grab exams from Gold

--- CS_Calendar/Model/Exam.cs
+++ CS_Calendar/Model/Exam.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,13 +8,22 @@ namespace CS_Calendar.Model
 {
     public class Exam
     {
+        private string course;
         private string name { get; set; }
         private MeetingTime meetingTime { get; set; }
 
-        public Exam(string name, MeetingTime meetingTime)
+        public Exam(string name, MeetingTime meetingTime, string course)
         {
             this.name = name;
+            this.course = course;
             this.meetingTime = meetingTime;
+        }
+
+        public static Exam createExams(IWebElement finals)
+        {
+            //returns an exam object created from an IWebElement
+            return null;
+           
         }
     }
 }
